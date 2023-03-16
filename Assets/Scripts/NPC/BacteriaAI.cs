b6eac06dfb357955c6aa9aaf0b9d367d2bb327fa
lblht <lehota.lubomir@gmail.com>
2023-03-16 15:41:19 +0100

    ergbgrk

--- Assets/Scripts/NPC/BacteriaAI.cs
+++ Assets/Scripts/NPC/BacteriaAI.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 public class BacteriaAI : MonoBehaviour
 {
     
-    [SerializeField] private Animator animator;   
+    [SerializeField] private Animator animator;             // referencia na komponent animátor
     [SerializeField] private NavMeshAgent navMeshAgent;     // referencia na NavMesh Agent
     [SerializeField] private float slowSpeed;               // rýchlosť ked hľadá jednlo
     [SerializeField] private float mediumSpeed;             // rýchlosť ked sa pohybuje k jednlu
@@ -21,7 +21,7 @@ public class BacteriaAI : MonoBehaviour
     [SerializeField] private int amountToReproduce;         // množstvo jedla ktoré musí skonzumovať na mitozu
     [SerializeField] private Transform mesh;                // referencia na transform meshu
     [SerializeField] private GameObject bacteriaPrefab;     // prefab baktérií ktoré sa spawnú po mitoze
-    [SerializeField] private LayerMask bacteriaLayer;   
+    [SerializeField] private LayerMask bacteriaLayer;       // vrstva na ktorej sa nachádzajú baktérie
 
     private Transform player;       // referencia na pozíciu hráča
     private Vector3 targetPosition; // pozícia ku ktorej sa práve pohybuje
@@ -31,28 +31,33 @@ public class BacteriaAI : MonoBehaviour
     private Food currentFood;       // referencia na momentálne jedlo
     private float eatTimer;         // uchováva hodnotu času ďalšieho odkusnutia
     private int resourceAmount;     // množstvo jedla ktoré zjedol
-    private float ragdollTimer;
+    private float ragdollTimer;     // čas ako dlho bude baktérie pod vplivom fyziky a nie voládaná komponentom NavMeshAgent
 
     void Start()
     {
-        GameManager.instance?.AddBacteria();
+        GameManager.instance?.AddBacteria();        // dá game managerovi vedieť že bola pridaná nová baktéria do hry
         state = States.LookingForFood;              // nastavenie stavu v ktorom začína
-        targetPosition = RandomTargetPosition();
-        navMeshAgent.enabled = false;
+        targetPosition = RandomTargetPosition();    // výber náhodnej pozície ku ktorj sa na začiatku začne pohybovať
+        navMeshAgent.enabled = false;               // na začiatku sa navmesh agent vypne aby aby bola baktéria od vplivom fyziky (aby padala z rany)
     }
 
     void FixedUpdate()
     {
-        if(CheckIfPlayerInRange() && state != States.Running)   // stále kontroluje či je vzdialenosť od hráča dostatočne malá
+         // stále kontroluje či je vzdialenosť od hráča dostatočne malá 
+         // ak je a ešte sa nenachádza v stave running tak sa do neho prepne
+        if(CheckIfPlayerInRange() && state != States.Running)
         {
-            state = States.Running;                             // ak ano prepne sa do stavu utekania
+            state = States.Running; // zmena stavu na stav running
+
+            // ak je v dostatočnej blízkosti hráča a je zameraný ne nejaké jedlo (predchádzajúci stav bol Eating)
             if(currentFood != null)
             {
-                currentFood.SetOccupied(false);
-                currentFood = null;
+                currentFood.SetOccupied(false); // jedlu treba oznámiť že už nieje jedené, aby ďalšia baktéria vedela že sa na ňom môže krmiť
+                currentFood = null;             // momentálne vybrané jedno baktérie nastavíme na null (nemá žiadne vybrané, uteká od hráča)
             }
         }
-        // ktorý podla momentálneho stavu volá príslušné funkcie
+
+        //switch ktorý podla momentálneho stavu volá príslušné funkcie v ktorých sa nachádza chovanie baktérie v danom stave
         switch (state)                  
         {
         case States.LookingForFood:
@@ -71,28 +76,32 @@ public class BacteriaAI : MonoBehaviour
             break;
         }
 
-        if(navMeshAgent.enabled == false && Time.time > ragdollTimer)     // dočasná poistka
+        // ak je komponent NavMeshAgent vypnutý a čas v ktorom je baktéria ovládaná fyzikov (ragdoll)v vypršal
+        // ragdoll sa vypne (zavolá sa funkcie Ragdoll s argumentom false)
+        // a vybereie sa nová náhodná pozícia ku ktorej sa začne baktérie pohybovať
+        if(navMeshAgent.enabled == false && Time.time > ragdollTimer)  
         {
             Ragdoll(false, 0f);
             targetPosition = RandomTargetPosition();
         }
 
+
+        // ak je NavMeshAgent zapnutý, jeho destinácia sa nastavuje na targetDestination
+        // som si celkom istý že to stačí nastaviť iba raz pri zmene destinácie a nie nastavovať stále v update
+        // takže neviem prečo to tu je ale nebudem to radšej meni kým všetko funguje :)
         if(navMeshAgent.enabled == true)
-            navMeshAgent.destination = targetPosition;      // nastavenie destinácie to NavMesh Agenta
-
-
-
-        AlignMeshWithGround();
+            navMeshAgent.destination = targetPosition;
     }
-    // funkcia volaná ak je v stave hladania jedla
+
+    // funkcia obsahujúca chovanie baktérie v stave LookingForFood
     void LookingForFood()
     {
-        navMeshAgent.speed = slowSpeed;
+        navMeshAgent.speed = slowSpeed; // nastavenie rýchlosti baktérie na pomalú
 
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, foodRadius, foodMask);  // získa pole colliderov v danom rádiuse ktoré su na vrstve food radius
         if(hitColliders.Length > 0)
         {
-            targetPosition = FindClosestFood(hitColliders);     // nájde najbližšie jedno
+            targetPosition = FindClosestFood(hitColliders);     // nájde najbližšie jedlo
             state = States.MovingToFood;                        // zmení stav na pohyb k jedlu
             return;
         }
@@ -103,15 +112,16 @@ public class BacteriaAI : MonoBehaviour
             moveTimer = Time.time + 10;
         }
     }
-    // funkcia volaná ak je v stavle presúvania sa k jedlu
+
+    // funkcia obsahujúca chovanie baktérie v stave MovingToFood
     void MovingToFood()
     {
-        navMeshAgent.speed = mediumSpeed;
+        navMeshAgent.speed = mediumSpeed; // nastavenie rýychlosti baktérie na strednú
 
         if(currentFood.GetOccupied())   // stále kontroluje či sa k jedlu nedostala skôr iná baktéria
         {
-            state = States.LookingForFood;
-            currentFood = null;
+            state = States.LookingForFood; // ak dsotala začne hladať iné jedno
+            currentFood = null; // vybrané jedno zmení na null
         }   
         else if(Vector3.Distance(targetPosition, transform.position) < 1.5f)    // ak je vzdialenosť k jedlu menšia ako 2 zmení sa stav na jedenie
         {
@@ -120,10 +130,11 @@ public class BacteriaAI : MonoBehaviour
             currentFood.SetOccupied(true);
         }
     }
-    // funkcia volaná ak je v stave jedenia
+
+    // funkcia obsahujúca chovanie baktérie v stave Eating
     void Eating()
     {
-        navMeshAgent.speed = 0;
+        navMeshAgent.speed = 0; // nastavenie rýchlosti baktérie na 0, nebohybuje sa pri kŕmení
 
         // Aby bola baktéria vždy otočená k jedlu ktoré práva papá
         Vector3 rotDir = Vector3.RotateTowards(transform.forward, currentFood.transform.position - transform.position, Time.deltaTime * 1f, 0.0f);
@@ -132,11 +143,13 @@ public class BacteriaAI : MonoBehaviour
         if(Time.time > eatTimer)
             TakeABite();
     }
+
     // Funkcia odoberie zdroje z objektu jedla a pridá zdroje (jedlo) baktérii
     void TakeABite()
     {
         if(currentFood.ReduceAmount(eatingAmount))  // Ak ma objekt jedla dostatok zdrojov
         {
+            // myslím že toto asi chápeme nechce sa mi to všerko komentovať :)
             resourceAmount += eatingAmount;
             if(resourceAmount >= amountToReproduce)
             {
@@ -151,6 +164,7 @@ public class BacteriaAI : MonoBehaviour
             currentFood = null;
         }
     }
+
     // funkcia sa stará o rozmnožovanie baktérie
     void Mitosis()
     {
--- Assets/Scripts/NPC/BacteriaDeath.cs
+++ Assets/Scripts/NPC/BacteriaDeath.cs
@@ -5,10 +5,10 @@ using UnityEngine.AI;
 
 public class BacteriaDeath : MonoBehaviour
 {
-    private int maxHp = 5;
-    [SerializeField] private int dropChance;
-    [SerializeField] private GameObject part;
+    [SerializeField] private int dropChance; // Šanca v % či z bakterie padne časť z jej teľa ( neskôr zbierané dendritic cell)
+    [SerializeField] private GameObject part; // prefab objektu časti baktérie 
 
+    private int maxHp = 5;
     private int currentHp;
 
     void Start()
@@ -16,23 +16,31 @@ public class BacteriaDeath : MonoBehaviour
         currentHp = maxHp;
     }
 
+    // funkcia na zabitie baktérie (používať túto nie iba Destroy()),
+    // vyžaduje argument názvu charakteru, ktorý ju zabil: "Macrophage", "Neutrophile", "TCell"... pre updatnutie správneho objectivu
     public void Die(string responsible)
     {
         Drop();
-        GameManager.instance?.RemoveBacteria(responsible);
+        GameManager.instance?.RemoveBacteria(responsible); // baktériu z hry treba vymazať týmto spôsobom - updatne ui, updatne counter atd...
         Destroy(gameObject);
     }
 
+    // funkcia urěná na poškodenie baktérie pre postavy ktoré ju nezabijú instantne
+    // taktiež vyždajuje okrem argumentu veľkosti poškodenia (demage), aj argument charakteru, ktorý ju zabil
     public void takeDamage(int damage, string responsible)
     {
         currentHp = currentHp-damage;
-        if (currentHp<=0)
+        if(currentHp <= 0)
         {
             Die(responsible);
         }
         Debug.Log(currentHp);
     }
 
+
+    // funkcia sa volá vždy iba pri smrti baktérie, vyberie náhodné číslo od 1 do 100
+    // ak je číslo menšie alebo rovné ako dropChance (šanca na dropnutie časti tela baktérie (dna))
+    // spawne inštanciu objektu dna baktérie
     private void Drop()
     {
         if(Random.Range (1, 101) <= dropChance)
